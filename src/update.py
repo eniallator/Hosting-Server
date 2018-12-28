db547752a4d120d0b70e82d1353f5d7f1329643e
eniallator <eniallatormc@gmail.com>
2018-12-28 11:03:08 +0000

    Implemented getting the files to download

--- src/update.py
+++ src/update.py
@@ -6,12 +6,30 @@ from json import loads
 SHA_FILE = 'SHA_FILE.txt'
 
 
-def update():
-    pass
+def get_files_to_download(download_url, to_download={}):
+    response = get(download_url)
+
+    if response.status_code < 200 or response.status_code >= 300:
+        return
+
+    repo_content = loads(response.content)
+
+    for dl_file in repo_content:
+        if dl_file['type'] == 'file':
+            to_download[dl_file['name']] = dl_file['download_url']
+        elif dl_file['type'] == 'dir':
+            to_download[dl_file['name']] = {}
+            get_files_to_download(dl_file['url'], to_download[dl_file['name']])
+
+    return to_download
 
 
 def update_project(path):
     response = get('https://api.github.com/repos/' + REPO + '/commits/' + BRANCH)
+
+    if response.status_code < 200 or response.status_code >= 300:
+        return
+
     content = loads(response.content)
     last_sha = content['commit']['tree']['sha']
     sha_file_path = os.path.join(path, SHA_FILE)
@@ -22,5 +40,7 @@ def update_project(path):
         return
 
     with open(sha_file_path, 'r') as file_handle:
-        if file_handle.read() != last_sha:
-            update()
+        if file_handle.read() == last_sha:
+            return
+
+    to_download = get_files_to_download('https://api.github.com/repos/' + REPO + '/contents?ref=' + BRANCH)
