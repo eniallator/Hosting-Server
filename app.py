e611d2b8d0a66596e99b972f553ded43043e8195
eniallator <eniallatormc@gmail.com>
2018-12-28 22:15:26 +0000

    Made it only update if it's in production mode

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from os import path
+import os
 from src.update import update_project
+
+try:
+    from dev_config import DEV_MODE
+except ModuleNotFoundError as err:
+    DEV_MODE = False
 
 
 def main():
-    update_project(path.dirname(path.realpath(__file__)))
+    if not DEV_MODE:
+        print('Checking for updates...')
+        update_project(os.path.dirname(os.path.realpath(__file__)))
 
 
 if __name__ == "__main__":
